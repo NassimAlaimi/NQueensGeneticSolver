f327cd445d46ed35a2ca40d08fe98f87dca2e351
Nassim Alaimi <nassim.alaimi@lisaqua.com>
2023-10-03 14:20:49 +0200

    faster algo

--- nqueen.py
+++ nqueen.py
@@ -1,142 +1,97 @@
 import random
-
-# Print all solutions
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
+import time
 
 def fitness(solution):
     n = len(solution)
-    conflicts = 0
-
-    # Check for conflicts in columns and diagonals
+    clashes = 0
     for i in range(n):
         for j in range(i + 1, n):
-            if solution[i] == solution[j] or abs(solution[i] - solution[j]) == abs(i - j):
-                conflicts += 1
+            if abs(i - j) == abs(solution[i] - solution[j]) or solution[i] == solution[j]:
+                clashes += 1
+    return clashes
 
-    # If there are no conflicts, it's a valid solution
-    if conflicts == 0:
-        return 1.0
-    else:
-        return 1.0 / (1.0 + conflicts)  # Higher conflicts lead to lower fitness
+def generate_population(pop_size, board_size):
+    return [random.sample(range(1, board_size + 1), board_size) for _ in range(pop_size)]
 
-# Select individuals for crossover
-def selection(population, fitness_function):
-    total_fitness = sum(fitness_function(solution) for solution in population)
+def select_parents(population, num_parents):
+    # Calculer les fitness pour chaque solution dans la population
+    fitness_scores = [fitness_cache.get(tuple(solution), fitness(solution)) for solution in population]
+    
+    # Sélectionner les indices des parents en fonction des fitness (plus faible est mieux)
+    selected_indices = sorted(range(len(fitness_scores)), key=lambda i: fitness_scores[i])[:num_parents]
+    
+    # Retourner les solutions des parents sélectionnés
+    return [population[i] for i in selected_indices]
 
-    # Calculate the probability of each individual being selected
-    selection_probabilities = [fitness_function(solution) / total_fitness for solution in population]
+def crossover(parent1, parent2):
+    n = len(parent1)
+    crossover_point = random.randint(1, n)
+    child = [0] * n
+    child[:crossover_point] = parent1[:crossover_point]
 
-    # Perform roulette wheel selection to choose the individuals
-    selected_population = []
-    for _ in range(len(population)):
-        selected_individual = roulette_wheel_select(population, selection_probabilities)
-        selected_population.append(selected_individual)
+    # Créer une liste des positions disponibles pour les gènes de l'enfant
+    available_positions = [x for x in parent2 if x not in child]
+    
+    for i in range(n):
+        if child[i] == 0:
+            if available_positions:
+                # Si des positions sont disponibles, sélectionner un gène parmi elles
+                gene = available_positions.pop()
+                child[i] = gene
+            else:
+                # Si la liste est vide, prendre un gène au hasard parmi les gènes non utilisés
+                remaining_genes = [x for x in range(1, n + 1) if x not in child]
+                random_gene = random.choice(remaining_genes)
+                child[i] = random_gene
 
-    return selected_population
+    return child
 
-# Select an individual using roulette wheel selection
-def roulette_wheel_select(population, probabilities):
-    # Spin the roulette wheel and select an individual
-    selected_index = 0
-    random_value = random.random()
-
-    while random_value > 0:
-        random_value -= probabilities[selected_index]
-        selected_index += 1
-
-    selected_index -= 1  # Adjust for overshooting
-
-    return population[selected_index]
-
-# Perform crossover on the selected individuals
-def crossover(selected_population, population_size):
-    offspring_population = []
-
-    # Perform crossover to create new offspring
-    while len(offspring_population) < population_size:
-        parent1, parent2 = random.sample(selected_population, 2)  # Select two parents randomly
-
-        # Choose a random crossover point
-        crossover_point = random.randint(1, len(parent1) - 1)
-
-        # Create offspring by combining genetic material from parents
-        child1 = parent1[:crossover_point] + parent2[crossover_point:]
-        child2 = parent2[:crossover_point] + parent1[crossover_point:]
-
-        # Add offspring to the new population
-        offspring_population.extend([child1, child2])
-
-    # Ensure the offspring population size matches the desired population size
-    if len(offspring_population) > population_size:
-        offspring_population = offspring_population[:population_size]
-
-    return offspring_population
-
-# Mutate the population
-def mutation(offspring_population, mutation_rate):
-    mutated_population = []
-
-    for solution in offspring_population:
+def mutate(solution, mutation_rate):
+    n = len(solution)
+    for i in range(n):
         if random.random() < mutation_rate:
-            # Apply mutation to this solution
-            mutated_solution = mutate_solution(solution)
-            mutated_population.append(mutated_solution)
-        else:
-            mutated_population.append(solution)
+            j = random.randint(0, n - 1)
+            solution[i], solution[j] = solution[j], solution[i]
 
-    return mutated_population
+def genetic_algorithm(board_size, pop_size, num_generations, mutation_rate):
+    population = generate_population(pop_size, board_size)
+    best_solution = min(population, key=lambda x: fitness_cache.get(tuple(x), fitness(x)))
+    best_fitness = fitness_cache.get(tuple(best_solution), fitness(best_solution))
 
-# Mutate a solution by randomly selecting a queen and moving it to a new row in the same column
-def mutate_solution(solution):
-    # Randomly select a queen and move it to a new row in the same column
-    mutated_solution = solution[:]
-    queen_to_mutate = random.randint(0, len(solution) - 1)
-    new_row = random.randint(0, len(solution) - 1)
-    mutated_solution[queen_to_mutate] = new_row
-    return mutated_solution
+    for generation in range(1, num_generations + 1):
+        parents = select_parents(population, pop_size // 2)
+        new_population = parents.copy()
 
-# Generate an initial population, this function will only be called once at the beginning of the algorithm
-def initial_population(population_size, n):
-    population = []
-    for _ in range(population_size):
-        solution = list(range(n))
-        random.shuffle(solution)
-        population.append(solution)
-    return population
+        while len(new_population) < pop_size:
+            parent1, parent2 = random.sample(parents, 2)
+            child = crossover(parent1, parent2)
+            mutate(child, mutation_rate)
+            new_population.append(child)
 
-# Main loop of the genetic algorithm
-def genetic_algorithm(n, population_size, max_generations, mutation_rate):
-    population = initial_population(population_size, n)
-    best_solution = None
-    best_fitness = 0
+        population = new_population
+        current_best = min(population, key=lambda x: fitness_cache.get(tuple(x), fitness(x)))
+        current_fitness = fitness_cache.get(tuple(current_best), fitness(current_best))
 
-    for generation in range(max_generations):
-        # Selection
-        selected_population = selection(population, fitness)
+        if current_fitness < best_fitness:
+            best_solution = current_best
+            best_fitness = current_fitness
 
-        # Crossover
-        offspring_population = crossover(selected_population, population_size)
+        print(f"Generation {generation}: {best_fitness} clashes")
 
-        # Mutation
-        mutated_population = mutation(offspring_population, mutation_rate)
-
-        # Replacement
-        population = mutated_population # We can use a function for the replacement step if we want to
-
-        # Track the best solution
-        for solution in population:
-            current_fitness = fitness(solution)
-            if current_fitness > best_fitness:
-                best_solution = solution
-                best_fitness = current_fitness
-
-        # If we find a solution with fitness 1 we can stop
-        if best_fitness == 1.0:
+        if best_fitness == 0:
+            print("Solution trouvée avec zéro conflit ! : ", best_solution)
             break
 
-    return best_solution, best_fitness, generation + 1
+    return best_solution
 
-# n is the size of the board, population_size is the number of solutions in each generation,max_generations is the maximum number of generations, the algorithm will stop if this number is reached, mutation_rate is the probability of a mutation occurring
-print(genetic_algorithm(n=32, population_size=100, max_generations=10000, mutation_rate=0.1))
+board_size = int(input("Taille du tableau: "))
+pop_size = int(input("Taille de la population: "))
+num_generations = int(input("Nombre de générations: "))
+mutation_rate = float(input("Taux de mutation: "))
+
+fitness_cache = {}
+
+start = time.time()
+solution = genetic_algorithm(board_size, pop_size, num_generations, mutation_rate)
+end = time.time()
+print(f"Solution: {solution} with {fitness(solution)} clashes in {end - start} seconds")
